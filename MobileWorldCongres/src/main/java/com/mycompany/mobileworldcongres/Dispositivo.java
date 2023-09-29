cbae92ed23eab033458dd9425f7b9940888c3237
joancrllp <2023_joan.cruz@iticbcn.cat>
2023-09-29 15:36:55 +0200

    Actualizacion

--- MobileWorldCongres/src/main/java/com/mycompany/mobileworldcongres/Dispositivo.java
+++ MobileWorldCongres/src/main/java/com/mycompany/mobileworldcongres/Dispositivo.java
@@ -1,20 +1,21 @@
 package com.mycompany.mobileworldcongres;
 
-public abstract class Dispositivo{
-    
+public class Dispositivo {
     private String marca;
-    private String modelo;
-    private double precioBase;
-    
-    public  Dispositivo(String marca, String modelo, double precioBase){
-        if (precioBase < 0){
-            throw new IllegalArgumentException("El precio no es correcto");
-        }
+    private String model;
+    private double preuBase;
+    private boolean accelerometre;
+    private boolean gps;
+
+    public Dispositivo(String marca, String model, double preuBase, boolean accelerometre, boolean gps) {
         this.marca = marca;
-        this.modelo = modelo;
-        this.precioBase = precioBase;
+        this.model = model;
+        this.preuBase = preuBase;
+        this.accelerometre = accelerometre;
+        this.gps = gps;
     }
 
+    // Getters i setters per cada atribut
     public String getMarca() {
         return marca;
     }
@@ -23,26 +24,41 @@ public abstract class Dispositivo{
         this.marca = marca;
     }
 
-    public String getModelo() {
-        return modelo;
+    public String getModel() {
+        return model;
     }
 
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
+    public void setModel(String model) {
+        this.model = model;
     }
 
-    public double getPrecioBase() {
-        return precioBase;
+    public double getPreuBase() {
+        return preuBase;
     }
 
-    public void setPrecioBase(double precioBase) {
-        this.precioBase = precioBase;
+    public void setPreuBase(double preuBase) {
+        this.preuBase = preuBase;
+    }
+
+    public boolean isAccelerometre() {
+        return accelerometre;
+    }
+
+    public void setAccelerometre(boolean accelerometre) {
+        this.accelerometre = accelerometre;
+    }
+
+    public boolean isGps() {
+        return gps;
+    }
+
+    public void setGps(boolean gps) {
+        this.gps = gps;
     }
 
     @Override
     public String toString() {
-        return "Dispositivo{" + "marca=" + marca + ", modelo=" + modelo + ", precioBase=" + precioBase + "€" + '}';
+        return "Marca: " + marca + "\nModel: " + model + "\nPreu Base: " + preuBase +
+               "\nAcceleròmetre: " + (accelerometre ? "Sí" : "No") + "\nGPS: " + (gps ? "Sí" : "No");
     }
-    
-    public abstract double preuFinal();
 }
